ce617bfb4f007ac016e930111b75a920fed24225
Kyle <kyle.mai@wiredcraft.com>
2023-07-27 12:37:37 +0800

    i

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,4 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-import { redirect } from 'next/navigation'
+import redirect from 'nextjs-redirect'
 
-export default function Home() {
-  redirect('https://booking-system-53634-7-1318564604.sh.run.tcloudbase.com')
-}
+export default redirect('https://booking-system-53634-7-1318564604.sh.run.tcloudbase.com')
+
