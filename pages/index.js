e7251a21734485740ab016b81afebdd55b05ae5f
Kyle <kyle.mai@wiredcraft.com>
2023-07-27 12:33:06 +0800

    i

--- pages/index.js
+++ pages/index.js
@@ -4,5 +4,5 @@ import Footer from '@components/Footer'
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return redirect('https://booking-system-53634-7-1318564604.sh.run.tcloudbase.com')
+  redirect('https://booking-system-53634-7-1318564604.sh.run.tcloudbase.com')
 }
